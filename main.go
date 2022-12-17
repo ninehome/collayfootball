67517a5e77acc8e4a90c386f3d5f29b472642cd6
nine <nine.h@iscmango.com>
2022-12-17 15:46:14 +0800

    爬虫

--- main.go
+++ main.go
@@ -1,29 +1,88 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/gocolly/colly/v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	// 创建 collector
-	collector := colly.NewCollector()
 
-	// 设置UA
-	//collector.UserAgent = USER_AGENT
+	dir, err := ioutil.TempDir("", "chromedp-example")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
 
-	// 事件监听
-	collector.OnRequest(func(r *colly.Request) {
-		fmt.Println("url:", r.URL.String())
-		// url: https://www.baidu.com/
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.DisableGPU,
+		chromedp.NoDefaultBrowserCheck,
+		chromedp.Flag("headless", false),
+		chromedp.Flag("ignore-certificate-errors", true),
+		chromedp.Flag("window-size", "50,400"),
+		chromedp.UserDataDir(dir),
+	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	// also set up a custom logger
+	taskCtx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	// create a timeout
+	//taskCtx, cancel = context.WithTimeout(taskCtx, 10*time.Second)
+	//defer cancel()
+
+	// ensure that the browser process is started
+	if err := chromedp.Run(taskCtx); err != nil {
+		panic(err)
+	}
+
+	// listen network event
+	listenForNetworkEvent(taskCtx)
+
+	chromedp.Run(taskCtx,
+		network.Enable(),
+		chromedp.Navigate(`https://test.f66b88sport.com/pc/index.html#/`),
+		chromedp.WaitVisible(`body`, chromedp.BySearch),
+		chromedp.Sleep(6*time.Second),
+	)
+
+	time.Sleep(30 * time.Second)
+
+}
+
+// 监听
+func listenForNetworkEvent(ctx context.Context) {
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		switch ev := ev.(type) {
+
+		case *network.EventResponseReceived:
+			resp := ev.Response
+
+			if len(resp.Headers) != 0 {
+				// log.Printf("received headers: %s", resp.Headers)
+
+				//if resp.URL == "https://sportapi.fastball2.com/v1/match/getList" {
+				//	fmt.Println("received headers: %s", resp.URL)
+				//	fmt.Println(resp)
+				//}
+
+				if strings.Index(resp.URL, ".ts") != -1 {
+					log.Printf("received headers: %s", resp.URL)
+					fmt.Println("received headers: %s", resp.URL)
+				}
+			}
+
+		}
+		// other needed network Event
 	})
-
-	// 解析元素
-	collector.OnHTML("title", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
-		// 百度一下，你就知道
-	})
-
-	// 访问网页
-	collector.Visit("https://www.huxiu.com/")
 }
